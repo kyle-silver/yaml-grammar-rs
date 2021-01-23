a36c6e4e3c5c07a844d562e98b3006ac7f9a3860
Kyle Silver <kyleAsilver@gmail.com>
2021-01-23 14:49:04 -0500

    refactoring

---
+++ src/obj.rs
@@ -0,0 +1,248 @@
+use std::collections::HashMap;
+
+use grammar::YamlParseResult;
+use lazy_static::lazy_static;
+use serde_yaml::{Mapping, Value};
+
+use crate::{grammar::{self, Constraint, PEType, ParseErr}, value_ref::ValueRef};
+use crate::valstr;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ObjConstr<'a> {
+    Fields(HashMap<&'a Value, Constraint<'a>>),
+    Any,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ObjectConstraint<'a> {
+    pub field_name: &'a Value,
+    constr: ObjConstr<'a>,
+    default: Option<&'a Mapping>,
+}
+
+impl<'a> ObjectConstraint<'a> {
+    pub fn default(field_name: &Value) -> ObjectConstraint {
+        ObjectConstraint { field_name, constr: ObjConstr::Any, default: None }
+    }
+
+    fn new(field_name: &'a Value, constr: ObjConstr<'a>, default: Option<&'a Mapping>) -> ObjectConstraint<'a> {
+        ObjectConstraint { field_name, constr, default }
+    }
+
+    pub fn add(&mut self, field_name: &'a Value, constraint: Constraint<'a>) {
+        if let ObjConstr::Fields(map) = &mut self.constr {
+            map.insert(&field_name, constraint);
+        }
+    }
+}
+
+impl<'a> From<ObjectConstraint<'a>> for YamlParseResult<'a> {
+    fn from(c: ObjectConstraint<'a>) -> Self {
+        YamlParseResult::Single(Ok(Constraint::Obj(c)))
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct ObjectConstraintBuilder<'a, 'b> {
+    field_name: &'a Value,
+    config: &'a Mapping,
+    path: &'b [&'a Value],
+    default: Option<&'a Mapping>
+}
+
+impl<'a, 'b> ObjectConstraintBuilder<'a, 'b> {
+    fn new(field_name: &'a Value, config: &'a Mapping, path: &'b [&'a Value]) -> Result<Self, ParseErr<'a>> {
+        let default = Self::field_default(config, path)?;
+        Ok(Self { field_name, config, path, default })
+    }
+
+    fn field_default(config: &'a Mapping, path: &'b [&'a Value]) -> Result<Option<&'a Mapping>, ParseErr<'a>> {
+        lazy_static! {
+            static ref DEFAULT: Value = valstr!("default");
+        }
+        if let Some(val) = config.get(&DEFAULT) {
+            match val {
+                Value::Mapping(m) => Ok(Some(m)),
+                _ => Err(ParseErr::new(path, PEType::InvalidDefault(val)))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn path(&self) -> Vec<&'a Value> {
+        let mut path = self.path.to_vec();
+        path.push(self.field_name);
+        path
+    }
+
+    fn from_mapping(&self) -> YamlParseResult<'a> {
+        lazy_static! {
+            static ref FIELDS: Value = valstr!("fields");
+        }
+        if let Some(val) = self.config.get(&FIELDS) {
+            return self.fields(val);
+        }
+        let default = ObjectConstraint::default(self.field_name);
+        Constraint::Obj(default).into()
+    }
+
+    fn fields(&self, fields: &'a Value) -> YamlParseResult<'a> {
+        if let Value::Mapping(f) = fields {
+            let mut path = self.path.to_vec();
+            path.push(self.field_name);
+            let (ok, err): (Vec<_>, Vec<_>) = f.iter()
+                .map(|(k, v)| Constraint::parse(k, v, &path))
+                .partition(YamlParseResult::all_ok);
+            // see if it was parsed without errors
+            if err.is_empty() {
+                let map = ok.into_iter()
+                    .flatten()
+                    .map(Result::unwrap)
+                    .map(|c| (c.field_name(), c))
+                    .collect();
+                let constr = ObjConstr::Fields(map);
+                ObjectConstraint::new(self.field_name, constr, self.default).into()
+            } else {
+                YamlParseResult::Multi(err.into_iter().flatten().collect())
+            }
+        } else {
+            ParseErr::new(&self.path(), PEType::IncorrectType(fields)).into()
+        }
+    }
+}
+
+impl<'a> ValueRef<'a, Mapping> {
+    fn new(value: &'a Value) -> Result<ValueRef<'a, Mapping>, PEType<'a>> {
+        match value {
+            Value::Mapping(literal) => Ok(ValueRef::Literal(literal)),
+            Value::Sequence(path) => ValueRef::abs_path(path),
+            _ => Err(PEType::IncorrectType(value)),
+        }
+    }
+}
+
+pub fn build<'a>(field_name: &'a Value, config: &'a Mapping, path: &[&'a Value]) -> YamlParseResult<'a> {
+    match ObjectConstraintBuilder::new(field_name, config, path) {
+        Ok(builder) => builder.from_mapping(),
+        Err(e) => e.into()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+
+    use super::*;
+    use crate::str::StringConstraint;
+    use crate::valstr;
+
+    #[test]
+    fn obj_constr_valid() {
+        // yaml
+        let raw = concat!(
+            "type: object\n",
+            "fields:\n",
+            "  hello: string\n",
+            "  world: string\n",
+            "  nested:\n",
+            "    type: object\n",
+            "    fields:\n",
+            "      foobar: string"
+        );
+
+        // expected structure in code
+        // inner object constraint
+        let name = valstr!("nested");
+        let mut nested = ObjectConstraint::new(&name, ObjConstr::Fields(HashMap::new()), None);
+        let name = valstr!("foobar");
+        nested.add(&name, Constraint::Str(StringConstraint::default(&name)));
+        // outer object constraint
+        let name = valstr!("f");
+        let mut expected = ObjectConstraint::new(&name, ObjConstr::Fields(HashMap::new()), None);
+        let name = valstr!("nested");
+        expected.add(&name, Constraint::Obj(nested));
+        let name = valstr!("hello");
+        expected.add(&name, Constraint::Str(StringConstraint::default(&name)));
+        let name = valstr!("world");
+        expected.add(&name, Constraint::Str(StringConstraint::default(&name)));
+
+        // parse yaml and validate
+        let config: Mapping = serde_yaml::from_str(raw).unwrap();
+        let name = valstr!("f");
+        let res = build(&name, &config, &vec![]);
+        if let YamlParseResult::Single(Ok(Constraint::Obj(obj))) = res {
+            assert_eq!(&String::from("f"), obj.field_name);
+            assert_eq!(expected, obj);
+            assert_eq!(None, obj.default);
+        } else {
+            panic!("parse of valid input failed");
+        }
+    }
+
+    #[test]
+    fn obj_constr_invalid() {
+        let raw = concat!(
+            "type: object\n",
+            "fields:\n",
+            "  hello: stringerino\n",
+            "  world: string\n",
+        );
+
+        let config: Mapping = serde_yaml::from_str(raw).unwrap();
+        let name = valstr!("parent");
+        let results = build(&name, &config, &vec![]).get();
+        assert_eq!(results.len(), 1);
+
+        let pe = results.into_iter().next()
+            .expect("expected one error")
+            .expect_err("First entry should be an error");
+        let (p1, p2) = (valstr!("parent"), valstr!("hello"));
+        let expected = ParseErr::new(&vec![&p1, &p2], PEType::UnknownType("stringerino"));
+        assert_eq!(expected, pe);
+    }
+
+    #[test]
+    fn obj_constr_multiple_invalid() {
+        let raw = concat!(
+            "type: object\n",
+            "fields:\n",
+            "  hello: stringerino\n",
+            "  world:\n",
+            "    type: string\n",
+            "    regex: ^\\d{{{{$",
+        );
+
+        let config: Mapping = serde_yaml::from_str(raw).unwrap();
+        let name = valstr!("parent");
+        let results = build(&name, &config, &vec![]).get();
+        assert_eq!(results.len(), 2);
+
+        let (p1, p2) = (valstr!("parent"), valstr!("hello"));
+        let hello_type_error = ParseErr::new(&vec![&p1, &p2], PEType::UnknownType("stringerino"));
+        assert!(results.contains(&Err(hello_type_error)));
+
+        let (p1, p2) = (valstr!("parent"), valstr!("world"));
+        let world_regex_error = ParseErr::new(&vec![&p1, &p2], Regex::new("^\\d{{{{$").unwrap_err().into());
+        assert!(results.contains(&Err(world_regex_error)));
+    }
+
+    #[test]
+    fn obj_empty_fields() {
+        let raw = concat!(
+            "type: object\n",
+            "fields:\n",
+        );
+        
+        let config: Mapping = serde_yaml::from_str(raw).unwrap();
+        let name = valstr!("parent");
+        let results = build(&name, &config, &vec![]).get();
+        assert_eq!(results.len(), 1);
+
+        let pe = results.into_iter().next()
+            .expect("expected one error")
+            .expect_err("First entry should be an error");
+        let expected = ParseErr::new(&vec![&name], PEType::IncorrectType(&Value::Null));
+        assert_eq!(expected, pe);
+    }
+}
---
+++ src/str.rs
@@ -0,0 +1,315 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use serde_yaml::{Mapping, Value};
+use std::ops::Deref;
+
+use crate::{grammar::{PEType, ParseErr}, rule::{RuleErrType, RuleEvalResult}, value_ref::{ValueRef, ValueResolutionErr}};
+use crate::valstr;
+
+// A wrapper type because Regex doesn't implement Eq or PartialEq. In fairness,
+// most of the time you would never want to use a regex as a key -- but really we
+// just want this for test coverage
+#[derive(Debug)]
+pub struct WrappedRegex(Regex);
+
+impl Deref for WrappedRegex {
+    type Target = Regex;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl PartialEq for WrappedRegex {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.as_str() == other.as_str()
+    }
+}
+
+impl Eq for WrappedRegex {}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum StrConstr<'a> {
+    Allowed(Vec<ValueRef<'a, String>>),
+    Disallowed(Vec<ValueRef<'a, String>>),
+    Regex(Box<WrappedRegex>),
+    Equals(ValueRef<'a, String>),
+    NotEquals(ValueRef<'a, String>),
+    Any,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct StringConstraint<'a> {
+    pub field_name: &'a Value,
+    pub constr: StrConstr<'a>,
+    pub default: Option<&'a String>,
+}
+
+impl<'a> StringConstraint<'a> {
+    pub fn default(field_name: &Value) -> StringConstraint {
+        StringConstraint { field_name, constr: StrConstr::Any, default: None }
+    }
+
+    fn new(field_name: &'a Value, constr: StrConstr<'a>, default: Option<&'a String>) -> StringConstraint<'a> {
+        StringConstraint { field_name, constr, default }
+    }
+}
+
+#[derive(Debug)]
+struct StringConstraintBuilder<'a, 'b> {
+    field_name: &'a Value,
+    config: &'a Mapping,
+    path: &'b [&'a Value],
+    default: Option<&'a String>
+}
+
+impl<'a, 'b> StringConstraintBuilder<'a, 'b> {
+    fn new(field_name: &'a Value, config: &'a Mapping, path: &'b [&'a Value]) -> Result<Self, ParseErr<'a>> {
+        let default = Self::field_default(config, path)?;
+        Ok(Self { field_name, config, path, default })
+    }
+
+    fn field_default(map: &'a Mapping, path: &'b [&'a Value]) -> Result<Option<&'a String>, ParseErr<'a>> {
+        lazy_static! {
+            static ref DEFAULT: Value = valstr!("default");
+        }
+        if let Some(val) = map.get(&DEFAULT) {
+            match val {
+                Value::String(s) => Ok(Some(s)),
+                _ => Err(ParseErr::new(path, PEType::InvalidDefault(val)))
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn from_mapping(&self) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+        lazy_static! {
+            static ref ALLOWED: Value = valstr!("allowed");
+            static ref DISALLOWED: Value = valstr!("disallowed");
+            static ref REGEX: Value = valstr!("regex");
+            static ref EQ: Value = valstr!("eq");
+            static ref NEQ: Value = valstr!("neq");
+            static ref DEFAULT: Value = valstr!("default");
+        }
+        if let Some(val) = self.config.get(&REGEX) {
+            return self.regex(val);
+        }
+        if let Some(val) = self.config.get(&ALLOWED) {
+            return self.allowed(val);
+        }
+        if let Some(val) = self.config.get(&DISALLOWED) {
+            return self.disallowed(val);
+        }
+        if let Some(val) = self.config.get(&EQ) {
+            return self.eq(val);
+        }
+        if let Some(val) = self.config.get(&NEQ) {
+            return self.neq(val);
+        }
+        if self.default.is_some() {
+            return Ok(StringConstraint::new(self.field_name, StrConstr::Any, self.default));
+        }
+        Ok(StringConstraint::default(self.field_name))
+    }
+
+    fn regex(&self, re: &'a Value) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+        if let Value::String(re) = re {
+            match Regex::new(re) {
+                Ok(regex) => Ok(StringConstraint { 
+                    field_name: self.field_name,
+                    constr: StrConstr::Regex(Box::new(WrappedRegex(regex))),
+                    default: self.default,
+                }),
+                Err(e) => Err(ParseErr::new(self.path, PEType::Regex(e)))
+            }
+        } else {
+            Err(ParseErr::new(self.path, PEType::IncorrectType(re)))
+        }
+    }
+
+    fn allowed(&self, allowed: &'a Value) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+        if let Value::Sequence(seq) = allowed {
+            let res = seq.iter().map(ValueRef::new).collect();
+            match res {
+                Ok(vals) => Ok(StringConstraint::new(self.field_name, StrConstr::Allowed(vals), self.default)),
+                Err(err) => Err(ParseErr::new(self.path, err)),
+            }
+        } else {
+            Err(ParseErr::new(self.path, PEType::IncorrectType(allowed)))
+        }
+    }
+
+    fn disallowed(&self, disallowed: &'a Value) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+        if let Value::Sequence(seq) = disallowed {
+            let res = seq.iter().map(ValueRef::new).collect();
+            match res {
+                Ok(vals) => Ok(StringConstraint::new(self.field_name, StrConstr::Disallowed(vals), self.default)),
+                Err(err) => Err(ParseErr::new(self.path, err)),
+            }
+        } else {
+            Err(ParseErr::new(self.path, PEType::IncorrectType(disallowed)))
+        }
+    }
+
+    fn eq(&self, to: &'a Value) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+        match ValueRef::new(to) {
+            Ok(vr) => Ok(StringConstraint::new(self.field_name, StrConstr::Equals(vr), self.default)),
+            Err(err) => Err(ParseErr::new(self.path, err)),
+        }
+    }
+
+    fn neq(&self, to: &'a Value) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+        match ValueRef::new(to) {
+            Ok(vr) => Ok(StringConstraint::new(self.field_name, StrConstr::NotEquals(vr), self.default)),
+            Err(err) => Err(ParseErr::new(self.path, err)),
+        }
+    }
+}
+
+impl<'a> ValueRef<'a, String> {
+    fn new(value: &'a Value) -> Result<ValueRef<'a, String>, PEType<'a>> {
+        match value {
+            Value::String(literal) => Ok(ValueRef::Literal(literal)),
+            Value::Sequence(path) => ValueRef::abs_path(path),    
+            _ => Err(PEType::IncorrectType(value))       
+        }
+    }
+}
+
+pub fn build<'a>(field_name: &'a Value, map: &'a Mapping, path: &[&'a Value]) -> Result<StringConstraint<'a>, ParseErr<'a>> {
+    StringConstraintBuilder::new(field_name, map, path)?.from_mapping()
+}
+
+#[derive(Debug)]
+pub enum StrRule<'a> {
+    Allowed(Vec<&'a String>),
+    Disallowed(Vec<&'a String>),
+    Regex(&'a WrappedRegex),
+    Equals(&'a String),
+    NotEquals(&'a String),
+    Any,
+}
+
+impl<'a> StrRule<'a> {
+    pub fn new(constr: &'a StrConstr, root: &'a Value) -> Result<StrRule<'a>, ValueResolutionErr<'a>> {
+        match constr {
+            StrConstr::Allowed(v) => {
+                let resolved: Result<_,_> = v.iter().map(|val| val.resolve(root)).collect();
+                Ok(StrRule::Allowed(resolved?))
+            }
+            StrConstr::Disallowed(v) => {
+                let resolved: Result<_,_> = v.iter().map(|val| val.resolve(root)).collect();
+                Ok(StrRule::Disallowed(resolved?))
+            }
+            StrConstr::Regex(re) => {
+                Ok(StrRule::Regex(re))
+            }
+            StrConstr::Equals(vr) => {
+                let resolved = vr.resolve(root)?;
+                Ok(StrRule::Equals(resolved))
+            }
+            StrConstr::NotEquals(vr) => {
+                let resolved = vr.resolve(root)?;
+                Ok(StrRule::NotEquals(resolved))
+            }
+            StrConstr::Any => {
+                Ok(StrRule::Any)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct StringRule<'a> {
+    field_name: &'a Value,
+    rule: StrRule<'a>,
+}
+
+impl<'a> StringRule<'a> {
+    pub fn new(constraint: &'a StringConstraint, root: &'a Value) -> Result<StringRule<'a>, ValueResolutionErr<'a>> {
+        match StrRule::new(&constraint.constr, root) {
+            Ok(rule) => Ok(StringRule {
+                field_name: constraint.field_name,
+                rule,
+            }),
+            Err(ValueResolutionErr::NotFound) => {
+                todo!("Implement search for default values in other rules")
+            }
+            Err(v) => Err(v)
+        }
+    }
+
+    pub fn eval(&self, value: &'a Value, path: &[&'a Value]) -> RuleEvalResult<'a> {
+        if let Value::String(x) = value {
+            match &self.rule {
+                StrRule::Allowed(list) => {
+                    RuleEvalResult::suc(list.contains(&x), path)
+                }
+                StrRule::Disallowed(list) => {
+                    RuleEvalResult::suc(!list.contains(&x), path)
+                }
+                StrRule::Regex(re) => {
+                    RuleEvalResult::suc(re.is_match(x), path)
+                }
+                StrRule::Equals(other) => {
+                    RuleEvalResult::suc(x == *other, path)
+                }
+                StrRule::NotEquals(other) => {
+                    RuleEvalResult::suc(x != *other, path)
+                }
+                StrRule::Any => {
+                    RuleEvalResult::suc(true, path)
+                }
+            }
+        } else {
+            RuleEvalResult::err(path, RuleErrType::IncorrectType(value))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use serde_yaml::Number;
+
+    use super::*;
+    use crate::lit;
+    use crate::valstr;
+    
+    #[test]
+    fn str_eq_valid() {
+        let raw = concat!(
+            "type: string\n",
+            "eq: hello",
+        );
+        let map: Mapping = serde_yaml::from_str(raw).unwrap();
+        let name = valstr!("f");
+        let acutal = build(&name, &map, &vec![]);
+        if let Ok(string_constraint) = acutal {
+            assert_eq!(string_constraint.field_name, &valstr!("f"));
+            assert_eq!(string_constraint.constr, StrConstr::Equals(lit!("hello")));
+            assert_eq!(string_constraint.default, None);
+        } else {
+            panic!("didn't parse valid input")
+        }
+    }
+    
+    #[test]
+    fn str_eq_invalid() {
+        let raw = concat!(
+            "type: string\n",
+            "eq: 7",
+        );
+        let map: Mapping = serde_yaml::from_str(raw).unwrap();
+        let name = valstr!("f");
+        let acutal = build(&name, &map, &vec![]);
+        if let Err(pe) = acutal {
+            assert_eq!(pe, ParseErr {
+                err: PEType::IncorrectType(&Value::Number(Number::from(7))),
+                path: Vec::<&Value>::new(),
+            })
+        } else {
+            panic!()
+        }
+    }
+}
